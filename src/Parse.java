55795e78d6f103ef8c3696fae876a1d337d24146
swhatelse <stevenwhatelse@hotmail.fr>
2017-02-09 10:53:16 -0700

    Almost working

--- src/Parse.java
+++ src/Parse.java
@@ -1,13 +1,27 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Stack;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class Parse {
+	
+	private static Stack<String> stack;
+	private static boolean debug = false;
 
+	public static void printDebug(String s){
+		if(debug)
+			System.out.println(s);
+	}
+	
+	public static void printRes(String s){
+		if(!debug)
+			System.out.print(s);
+	}
+	
 	public static void String(String s){
 		Pattern pexpr = Pattern.compile(Expression.REG_EXPR);
 		Pattern pstr_pr = Pattern.compile(Expression.REG_STRING_PRIME);
@@ -15,7 +29,6 @@ public class Parse {
 	    Matcher mstr_pr = pstr_pr.matcher(s);
 	    
 	    if(mexpr.find()){
-	    	//System.out.println("Expr = " + mexpr.group());
 	    	Expr(mexpr.group());
 	    }
 	    else{
@@ -23,67 +36,80 @@ public class Parse {
 	    }
 	    
 	    if(mstr_pr.find()){
-	    	//System.out.println("String' = " + mstr_pr.group());
 	    	String_Prime(mstr_pr.group());
 	    }		
 	}
 	
 	public static void String_Prime(String s){
-		
+    	printDebug("String' = " + s);
 	}
 	
 	public static void Expr(String s){
+		printDebug("Expr = " + s);
+		
 		Pattern ppre = Pattern.compile(Expression.REG_PRE);
-		Pattern pexpr_pr = Pattern.compile("("+Expression.REG_BINOP + Expression.REG_PRE+")+");
 	    Matcher mpre = ppre.matcher(s);
-	    Matcher mexpr_pr = pexpr_pr.matcher(s);
 	    
 	    if(mpre.find()){
+	    	printDebug("pre = " + mpre.group());
 	    	Pre(mpre.group());
-	    }
-	    
-	    if(mexpr_pr.find()){
-	    	Expr_Prime(mexpr_pr.group());
+	    	String part2 = s.substring(mpre.group().length());
+	    	Expr_Prime(part2);
 	    }
 	}
 	
 	public static void Expr_Prime(String s){
-		
+    	printDebug("expr' = " + s);
+		Pattern pexpr_pr = Pattern.compile("("+Expression.REG_BINOP + Expression.REG_PRE+")");
+		Matcher mexpr_pr = pexpr_pr.matcher(s);
+		if(mexpr_pr.find()){
+			Pre(mexpr_pr.group());
+			stack.push(mexpr_pr.group().charAt(0) + " ");
+			if(!stack.empty()){
+				printRes(stack.pop());
+			}
+			Expr_Prime(s.substring(mexpr_pr.group().length()));
+		}
 	}
 	
 	public static void Pre(String s){
-		//Pattern pincrop = Pattern.compile(Expression.REG_INCROP + Expression.REG_PRE);
-		//Matcher mincrop = pincrop.matcher(s);
+		printDebug("pre = " + s);
+		String part2 = s;
+		Pattern pincrop = Pattern.compile("^" + Expression.REG_INCROP);
+		Matcher mincrop = pincrop.matcher(part2);
+		Stack<String> pre_stack = new Stack<String>();
+		while(mincrop.find()){
+			pre_stack.push(mincrop.group());
+			part2 = part2.substring(mincrop.group().length());
+			mincrop = pincrop.matcher(part2);
+		}
+		
 		Pattern ppost = Pattern.compile(Expression.REG_POST);
 		Matcher mpost = ppost.matcher(s);
 		// As it is recursive we need to consume each pre incrop token
 		// until there are no more left
         if(mpost.find()){
-			Post(s);
+			Post(part2);
 		}
-		
-        String part2 = s;
-		Pattern pincrop = Pattern.compile("^" + Expression.REG_INCROP + "+");
-		Matcher mincrop = pincrop.matcher(part2);
-		while(mincrop.find()){
-			System.out.print(mincrop.group() + "_");
-			part2 = part2.substring(mincrop.group().length());
-			mincrop = pincrop.matcher(part2);
-		}
+        
+        while(!pre_stack.empty()){
+        	printRes(pre_stack.pop() + "_ ");
+        }
 	}
 	
 	public static void Post(String s){
+		printDebug("post = " + s);
 		Pattern pref = Pattern.compile(Expression.REG_REF);
 		Matcher mref = pref.matcher(s);
 		String part2;
 		if(mref.find()){
+			part2 = s.substring(mref.group().length());
 			Ref(mref.group());
-			part2 = mref.group().substring(mref.group().length());
-			
-			Pattern pincrop = Pattern.compile(Expression.REG_INCROP + "+");
+
+			Pattern pincrop = Pattern.compile(Expression.REG_INCROP);
 			Matcher mincrop = pincrop.matcher(part2);
 			while(mincrop.find()){
-				System.out.print("_" + mincrop.group());
+				printRes("_" + mincrop.group() + " ");
 				part2 = part2.substring(mincrop.group().length());
 				mincrop = pincrop.matcher(part2);
 			}
@@ -91,7 +117,8 @@ public class Parse {
 	}
 	
 	public static void Ref(String s){
-		Pattern plval = Pattern.compile("^F+");
+		printDebug("ref = " + s);
+		Pattern plval = Pattern.compile("^F");
 		Matcher mlval = plval.matcher(s);
 		
 		Pattern pterm = Pattern.compile(Expression.REG_TERM);
@@ -101,16 +128,24 @@ public class Parse {
 		}
 
 		while(mlval.find()){
-			System.out.print(mlval.group());
+			printRes(mlval.group() + " ");
 		}
 	}
 	
 	public static void Term(String s){
+		printDebug("term = " + s);
 		Pattern pnum = Pattern.compile("^" + Expression.REG_NUM);
 		Matcher mnum = pnum.matcher(s);
 		
+		Pattern ppar = Pattern.compile("^" + Expression.REG_EXPR_PAR);
+		Matcher mpar = ppar.matcher(s);
+		
 		if(mnum.find()){
-			System.out.print(mnum.group());
+			printRes(mnum.group() + " ");
+		}
+		else if(mpar.find()){
+			String part2 = mpar.group().substring(1, mpar.group().length() - 1);
+			Expr(part2);
 		}
 	}
 	/**
@@ -118,13 +153,17 @@ public class Parse {
 	 */
 	public static void main(String[] args) {
 		try {
-			Scanner sc = new Scanner(new File("test")).useDelimiter("\\s*\n\\s*");
+			String filename = args[0]; 
+			Scanner sc = new Scanner(new File(filename)).useDelimiter("\\s*\n\\s*");
 			Pattern p = Pattern.compile(Expression.REG_STRING);
 			while(sc.hasNext()){
+				stack = new Stack<String>();
 				String s = sc.next();
 				Matcher m = p.matcher(s);
-				if(m.matches()){					
+				if(m.matches()){		
+					System.out.print(s + " = ");
 					String(s);
+					System.out.println();
 				}
 				else{	
 					System.out.println("Error: not a string");
